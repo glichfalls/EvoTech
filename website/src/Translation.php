92ef6486ebafbe7ca7c77f5d61a4c1ed69e0f759
Robin Portner <robin.portner@jkweb.ch>
2023-01-23 21:43:54 +0100

    changes

---
+++ website/src/Translation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EvoTech;
+
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Translator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class Translation
+{
+
+    private string $locale;
+    private TranslatorInterface $translator;
+
+    public function __construct($locale)
+    {
+        $this->locale = $locale;
+        $translator = new Translator($locale);
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', sprintf('%s/translations/%s.yaml', $_SERVER['DOCUMENT_ROOT'], $locale), $locale);
+        $translator->setFallbackLocales(['de']);
+        $this->translator = $translator;
+    }
+
+    public function locale()
+    {
+        return $this->locale;
+    }
+
+    public function oppositeLocale()
+    {
+        if ($this->locale === 'de') {
+            return 'en';
+        } else {
+            return 'de';
+        }
+    }
+
+    public function t($path)
+    {
+        return $this->translator->trans($path);
+    }
+
+}
---
+++ website/tsc/impressum.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="../assets/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="../assets/code.js"></script>
+  <title>TopSpeedCenter Solothurn</title>
+</head>
+<body>
+
+  <?php
+    include '../../templates/header_tsc.php';
+  ?>
+
+  <main id="main-impressum">
+    <h1>impressum</h1>
+    <p>
+    TopSpeedCenter GmbH<br>
+    Glutz-Blotzheim-Strasse 3<br>
+    4500 Solothurn<br><br>
+    Geschäftsleitung:<br>
+    Christoph Flury<br>
+    Barbara Flury<br>
+    Cyrille Flury<br>
+    info@topspeedcenter.ch<br><br>
+    Handelsregisteramt des Kanton Berns<br>
+    Firmen-Nr.: CHE-198.529.402<br>
+    MwSt-Nr.: CHE-198.529.402
+    </p>
+  </main>
+
+</body>
+</html>
