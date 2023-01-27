ff6a674ba70fc46a525c1241228a06682cc12d31
robiningold <robin.ingold@hotmail.com>
2023-01-27 23:43:35 +0100

    translation

--- website/src/Translation.php
+++ website/src/Translation.php
@@ -17,7 +17,7 @@ class Translation
         $this->locale = $locale;
         $translator = new Translator($locale);
         $translator->addLoader('yml', new YamlFileLoader());
-        $translator->addResource('yml', sprintf('%s/translations/%s.yaml', $_SERVER['DOCUMENT_ROOT'], $locale), $locale);
+        $translator->addResource('yml', sprintf('/translations/%s.yaml', $_SERVER['DOCUMENT_ROOT'], $locale), $locale);
         $translator->setFallbackLocales(['de']);
         $this->translator = $translator;
     }
